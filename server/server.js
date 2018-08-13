2374e1d681a6a81c607bb0cdd0ba305afc1aa7fb
Paul Heggeseth <pheggeseth@gmail.com>
2018-08-13 16:17:54 -0500

    modified port for heroku

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json()); // This line is required for Angular
 // MONGODB SETUP
 const mongoose = require('mongoose');
 const databaseName = 'AngularToDo';
-const mongoURI = `mongodb://localhost:27017/${databaseName}`; // 27017 is the PORT that Mongo is running on
+const mongoURI = process.env.MONGODB_URI || `mongodb://localhost:27017/${databaseName}`; // 27017 is the PORT that Mongo is running on
 mongoose.connect(mongoURI, {useNewUrlParser: true}); // {useNewUrlParser: true} <- avoids a warning in the console
 mongoose.connection.on('open', () => console.log('Connected to Mongo'));
 mongoose.connection.on('error', error => console.log('ERROR CONNECTING TO MONGO', error));
