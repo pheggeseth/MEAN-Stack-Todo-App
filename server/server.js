5a332a4efa8df608480e20fb4f83b7a7a986e66e
Paul Heggeseth <pheggeseth@gmail.com>
2018-08-13 16:32:52 -0500

    moved mongoose into module

--- server/server.js
+++ server/server.js
@@ -2,18 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+require('./database-connection.js');
 
 // Configure body-parser for Angular and jQuery
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // This line is required for Angular
-
-// MONGODB SETUP
-const mongoose = require('mongoose');
-const databaseName = 'AngularToDo';
-const mongoURI = process.env.MONGODB_URI || `mongodb://localhost:27017/${databaseName}`; // 27017 is the PORT that Mongo is running on
-mongoose.connect(mongoURI, {useNewUrlParser: true}); // {useNewUrlParser: true} <- avoids a warning in the console
-mongoose.connection.on('open', () => console.log('Connected to Mongo'));
-mongoose.connection.on('error', error => console.log('ERROR CONNECTING TO MONGO', error));
 
 // import routes from router
 const todosRouter = require('./routes/todos.router.js');
